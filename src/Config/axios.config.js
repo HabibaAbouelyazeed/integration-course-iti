5fa60a68fa907bea5d43cd51e5121c816fb407f4
Habiba Mahmoud <habibaabouelyazeed@gmail.com>
2023-11-04 22:47:31 +0200

    interceptor

--- src/Config/axios.config.js
+++ src/Config/axios.config.js
@@ -6,3 +6,29 @@ export const server = axios.create({
         "Content-Type": 'application/json',
     }
 })
+
+server.interceptors.request.use(
+    (config) =>{
+        const accessToken = JSON.parse(localStorage.getItem('token'));
+        if(accessToken){
+            if(config.headers){
+                config.headers.token = accessToken;
+            }
+        }
+        return config;
+    },
+    (error) =>{
+        return Promise.reject(error)
+    }
+);
+
+server.interceptors.response.use(
+    (response) =>{
+        console.log('response: ', response);
+        return response;
+    },
+
+    (error) => {
+        return Promise.reject(error);
+    }
+);
